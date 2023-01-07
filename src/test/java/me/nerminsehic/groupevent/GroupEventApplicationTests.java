e4a7d46a80098140691e5b48176cbcdacd3aa7da
Nermin <sehicnermin97@gmail.com>
2023-01-07 16:06:14 +1100

    Remove SpringBootTest

--- src/test/java/me/nerminsehic/groupevent/GroupEventApplicationTests.java
+++ src/test/java/me/nerminsehic/groupevent/GroupEventApplicationTests.java
@@ -1,9 +1,7 @@
 package me.nerminsehic.groupevent;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class GroupEventApplicationTests {
 
     @Test
