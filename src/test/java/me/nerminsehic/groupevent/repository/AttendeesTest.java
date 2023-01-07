2bf2e9216e80b0c20bb2aad8d66747a370efc200
Nermin <sehicnermin97@gmail.com>
2023-01-07 14:12:41 +1100

    Write tests for Attendees repository

---
+++ src/test/java/me/nerminsehic/groupevent/repository/AttendeesTest.java
@@ -0,0 +1,52 @@
+package me.nerminsehic.groupevent.repository;
+
+import me.nerminsehic.groupevent.entity.Attendee;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+class AttendeesTest {
+
+    @Autowired
+    private Attendees underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void shouldFindAttendeeByEmailAddress() {
+        // given
+        String emailAddress = "test.test@email.com";
+        Attendee attendee = underTest.save(new Attendee(
+                emailAddress
+        ));
+
+        // when
+        Optional<Attendee> result = underTest.findByEmailAddress(emailAddress);
+
+        // then
+        assertThat(result).isPresent();
+    }
+
+    @Test
+    void shouldNotFindAttendeeByNonExistingEmailAddress() {
+        // given
+        String nonExistingEmailAddress = "test.test@email.com";
+
+        // when
+        Optional<Attendee> result = underTest.findByEmailAddress(nonExistingEmailAddress);
+
+        // then
+        assertThat(result).isNotPresent();
+    }
+}
